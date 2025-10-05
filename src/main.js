849e4527336547597d14a55f8840ff6bb30bfa49
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-10-06 00:57:02 +0200

    Fix hamburger menu on click navbar

--- src/main.js
+++ src/main.js
@@ -3,13 +3,7 @@ document.querySelector('#current-year').innerHTML = '' + new Date().getFullYear(
 const hamburger = document.getElementById('hamburger');
 const navMenu = document.getElementById('nav-menu');
 
-hamburger.addEventListener('click', () => {
+const hideHamburger = () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('show');
-});
-
-
-const hideHamburger = () => {
-    navMenu.classList.toggle('show');
-
 }
